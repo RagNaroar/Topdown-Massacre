15a602b7fb756fbeb978bbef496644116619119c
RagnaR <shaxnurmurodov1990@gmail.com>
2026-08-04 00:03:05 +0500

    Feat Add weapon sound

---
+++ Assets/Scripts/WeaponController/GunShotAudio.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GunShotAudio : MonoBehaviour
+{
+    public AudioClip shotClip;
+    public AudioSource audioSource;
+
+    void Update()
+    {
+        if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            PlayShotSound();
+        }
+    }
+
+    void PlayShotSound()
+    {
+        if (shotClip == null || audioSource == null) return;
+        audioSource.PlayOneShot(shotClip);
+    }
+}
